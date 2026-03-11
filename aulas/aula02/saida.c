aa810d8c88e717e1d74ccf09995b4d5626a0e4d2
unknown <2612082017@ACADEMICO.IESB.BR>
2026-03-11 10:58:13 -0300

    adicina codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,21 @@
+/*
+
+
+
+*/
+
+// inclusão de bibliotecas da linguangen
+#include <stdio.h> 
+
+int main() {
+  //1. Entrada
+
+  //2. Processamento
+
+  //3. Saida
+  printf("um texto qualquer\n");
+  printf("progamar em C eh Top\n");
+ // printf("%i\n", 10);
+  //printf("%i\n". 10.3455);
+    return 0;
+}
